272135b18a24d0a784b84dcd757783d9a765dd63
chickenosaurus5 <arabidzedato8@gmail.com>
2025-06-24 01:48:49 +0400

    added main banner on store page with animation

--- js/source.js
+++ js/source.js
@@ -10,6 +10,7 @@ hearts.forEach(function (heart) {
 // store page game banner controler
 
 let banners = document.querySelectorAll(".game-banner");
+let bannerImgs = document.querySelectorAll('.small-game-img')
 
 let intId = setInterval(() => {
   banners[0].click();
@@ -28,9 +29,15 @@ for (const [index, banner] of banners.entries()) {
       banners.forEach(function (banner) {
         banner.classList.remove("game-banner-clicked");
         banner.firstElementChild.classList.remove("loading-div-anim");
+        
       });
+      bannerImgs.forEach(function(bannerImg){
+        bannerImg.classList.remove('small-game-img-selected');
+      })
       banner.classList.add("game-banner-clicked");
       banner.firstElementChild.classList.add("loading-div-anim");
+      bannerImgs[index].classList.add('small-game-img-selected');
+
     }
   });
 }
@@ -39,9 +46,12 @@ for (let i = 0; i < 4; i++) {
   banners[i].firstElementChild.addEventListener("animationend", () => {
     banners[i].classList.remove("game-banner-clicked");
     banners[i].firstElementChild.classList.remove("loading-div-anim");
+    bannerImgs[i].classList.remove('small-game-img-selected');
     if (i + 1 < 4) {
+      banners[i+1].click()
       banners[i + 1].classList.add("game-banner-clicked");
-      banners[i + 1].firstElementChild.classList.add("loading-div-anim");
+      banners[i + 1].firstElementChild.classList.add("loading-div-anim"); 
+      bannerImgs[i+1].classList.add('small-game-img-selected');
     }
   });
 }
