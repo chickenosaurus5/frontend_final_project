5224b8eb1d962757cf8f1355bcc2284fc6cd6ac0
chickenosaurus5 <arabidzedato8@gmail.com>
2025-06-27 18:04:20 +0400

    finished storepage only burgerbar left

--- js/api.js
+++ js/api.js
@@ -1,52 +1,199 @@
+let apiKey = "658b246e22d347f4b8c499b4cb1644ab";
+let apiUrl = "https://api.rawg.io/api/games";
 
-let apiKey = '579dccf31bc94242a6342e0eaed1943c'
-let apiUrl = 'https://api.rawg.io/api/games'
+// render funqciebi elementebistvis
+function renderMainBanner() {
+  let games = document.querySelectorAll("#game");
+  let nextGames = document.querySelectorAll(".small-game-img");
+  let gameTitles = document.querySelectorAll(".game-title");
 
+  const gamesApi = JSON.parse(sessionStorage.getItem(`page-3-response`));
 
-
-
-function renderMainBanner(){
-    let games = document.querySelectorAll('#game')
-    let nextGames = document.querySelectorAll('.small-game-img')
-    let gameTitles = document.querySelectorAll('.game-title')
-
-    const gamesApi =  JSON.parse(sessionStorage.getItem('response'))
-
-    for(let i = 0; i < 4; i++){
-        games[i].src = `${gamesApi[i].background_image}`
-        nextGames[i].src = `${gamesApi[i].background_image}`
-        gameTitles[i].textContent = `${gamesApi[i].name}`
-    }
+  for (let i = 0; i < 4; i++) {
+    games[i].src = `${gamesApi[i].background_image}`;
+    nextGames[i].src = `${gamesApi[i].background_image}`;
+    gameTitles[i].textContent = `${gamesApi[i].name}`;
+  }
 }
 
-function renderGameCards(){
+function renderGameCards(wrapperIndex, startGameIndex, endGameIndex) {
+  let gamesWrapper = document.querySelectorAll(".swiper-wrapper");
+  const gamesApi = JSON.parse(sessionStorage.getItem(`page-3-response`));
 
-    let gameBanners = document.querySelectorAll('#game-banner-img')
-    let gameCardTitles = document.querySelectorAll('#game-card-title')
+  for (let i = startGameIndex; i < endGameIndex; i++) {
+    let offerGameBanner = document.createElement("div");
+    let gameBanner = document.createElement("img");
+    let gameInfoDiv = document.createElement("div");
+    let gameCardTitle = document.createElement("h2");
+    let gameCardPrice = document.createElement("p");
+
+    gameCardPrice.id = "game-card-price";
+    offerGameBanner.classList.add("swiper-slide", "offer-game-banner");
+    gameInfoDiv.classList.add("game-info");
+    gameBanner.id = "game-banner-img";
+
+    gameBanner.src = `${gamesApi[i].background_image}`;
+    gameBanner.alt = `${gamesApi[i].slug}`;
+    gameCardTitle.textContent = `${gamesApi[i].name}`;
+
+    if (sessionStorage.getItem(`${gamesApi[i].name}-priceResponse`) === null) {
+      fetch(
+        `https://www.cheapshark.com/api/1.0/deals?title=${gamesApi[i].name}&exact=1`,
+        {
+          method: "GET",
+        }
+      )
+        .then((res) => res.json())
+        .then((priceData) => {
+          console.log("fetch called");
+          if (!priceData.length) {
+            sessionStorage.setItem(`${gamesApi[i].name}-priceResponse`, "free");
+            gameCardPrice.textContent = "free";
+          } else {
+            sessionStorage.setItem(
+              `${gamesApi[i].name}-priceResponse`,
+              priceData[0].normalPrice
+            );
+            gameCardPrice.textContent = `${priceData[0].normalPrice}$`;
+          }
+
+          if (wrapperIndex === 0) {
+            if (gameCardPrice.textContent === "free") {
+              gameCardPrice.style.color = "rgb(50,205,50)";
+              gameCardPrice.textContent = `free`;
+            } else {
+              gameCardPrice.style.color = "rgb(50,205,50)";
+              gameCardPrice.textContent = `${(
+                parseFloat(gameCardPrice.textContent) -
+                parseFloat(gameCardPrice.textContent) * 0.3
+              ).toFixed(2)}$`;
+            }
+          }
+        });
+    } else {
+      let gamePrice = sessionStorage.getItem(
+        `${gamesApi[i].name}-priceResponse`
+      );
+      if (gamePrice === "free") {
+        gameCardPrice.textContent = `${gamePrice}`;
+      } else {
+        gameCardPrice.textContent = `${gamePrice}$`;
+      }
+
+      if (wrapperIndex === 0) {
+        if (gameCardPrice.textContent === "free") {
+          gameCardPrice.style.color = "rgb(50,205,50)";
+          gameCardPrice.textContent = `free`;
+        } else {
+          gameCardPrice.style.color = "rgb(50,205,50)";
+          gameCardPrice.textContent = `${(
+            parseFloat(gameCardPrice.textContent) -
+            parseFloat(gameCardPrice.textContent) * 0.3
+          ).toFixed(2)}$`;
+        }
+      }
+    }
+
+    gameCardTitle.id = "game-card-title";
+
     
-    const gamesApi =  JSON.parse(sessionStorage.getItem('response'))
-    for(let i = 4; i < 12; i++){
-        gameBanners[i-4].src = `${gamesApi[i].background_image}`
-        gameCardTitles[i-4].textContent = `${gamesApi[i].name}`
-        // nextGames[i].src = `${gamesApi[i].background_image}`
-        // gameTitles[i].textContent = `${gamesApi[i].name}`
-    }
+    gameInfoDiv.appendChild(gameCardTitle);
+    gameInfoDiv.appendChild(gameCardPrice);
+
+    offerGameBanner.appendChild(gameBanner);
+    offerGameBanner.appendChild(gameInfoDiv);
+
+    gamesWrapper[wrapperIndex].appendChild(offerGameBanner);
+  }
 }
 
-if (sessionStorage.getItem('response') === null){
-    console.log(sessionStorage.getItem('response'))
-    fetch(`${apiUrl}?key=${apiKey}&page=${3}`,{
-    method: 'GET',
-  }).then(res => res.json())
-    .then(data => {
-        sessionStorage.setItem('response',JSON.stringify(data.results))
-        renderMainBanner();
-        renderGameCards();
-    })
+function renderGameColumns(){
+    let gameCardCount = 0
+    let columnIndex = 0
+    //mtavari columnebi sadac tamashebi chaismeba 
+    let gameColumns = document.querySelectorAll('.game-column')
+    const gamesApi = JSON.parse(sessionStorage.getItem(`page-1-response`));
 
-}else{
-    renderMainBanner();
-    renderGameCards();
+    for(let i = 0; i < 12; i++){
+        
+        let  colGameCard = document.createElement("div");
+        let colGameImg = document.createElement("img");
+        let colGameInfoDiv = document.createElement("div");
+        let gameCardTitle = document.createElement("h2");
+        let gameGenre = document.createElement("p");
+
+        colGameCard.classList.add('col-game-card')
+        colGameImg.id = 'col-game-img'
+        colGameInfoDiv.classList.add('col-game-info')
+        gameCardTitle.id = 'col-game-nm'
+        gameGenre.id = 'col-game-genre'
+
+        colGameImg.src = `${gamesApi[i].background_image}`;
+        gameCardTitle.textContent = `${gamesApi[i].name}`;
+
+        for(let j = 0; j <gamesApi[i].genres.length; j++){
+            gameGenre.textContent += `${gamesApi[i].genres[j].name}; `
+        }
+
+
+
+        colGameInfoDiv.appendChild(gameCardTitle);
+        colGameInfoDiv.appendChild(gameGenre);
+
+        colGameCard.appendChild(colGameImg);
+        colGameCard.appendChild(colGameInfoDiv);
+        
+        if(gameCardCount < 4){
+            gameColumns[columnIndex].appendChild(colGameCard);
+            gameCardCount++;
+           
+        }else{
+            gameCardCount = 0;
+            columnIndex++;
+            gameColumns[columnIndex].appendChild(colGameCard); 
+            gameCardCount++;
+        }
+        
+        
+        
+    }
+
+    
+
+}
+
+// page 1 response
+if (sessionStorage.getItem(`page-1-response`) === null) {
+  fetch(`${apiUrl}?key=${apiKey}&page=${1}`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      sessionStorage.setItem(`page-1-response`, JSON.stringify(data.results));
+        renderGameColumns()
+
+    });
+} else {
+    renderGameColumns()
+}
+
+
+// page 3 response
+if (sessionStorage.getItem(`page-3-response`) === null) {
+  fetch(`${apiUrl}?key=${apiKey}&page=${3}`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      sessionStorage.setItem(`page-3-response`, JSON.stringify(data.results));
+      renderMainBanner();
+      renderGameCards(0, 4, 12);
+      renderGameCards(1, 13, 20);
+    });
+} else {
+  renderMainBanner();
+  renderGameCards(0, 4, 12);
+  renderGameCards(1, 13, 20);
 }
 
 
--- js/source.js
+++ js/source.js
@@ -7,13 +7,10 @@ hearts.forEach(function (heart) {
   });
 });
 
-
-
 // store page game banner controler
 
-
 let banners = document.querySelectorAll(".game-banner");
-let bannerImgs = document.querySelectorAll('.small-game-img')
+let bannerImgs = document.querySelectorAll(".small-game-img");
 let currg = document.querySelector(".current-game-div");
 
 let intId = setInterval(() => {
@@ -21,7 +18,6 @@ let intId = setInterval(() => {
 }, 20000);
 
 for (const [index, banner] of banners.entries()) {
- 
   banner.addEventListener("click", function () {
     if (!banner.classList.contains("game-banner-clicked")) {
       clearInterval(intId);
@@ -32,19 +28,18 @@ for (const [index, banner] of banners.entries()) {
       banners.forEach(function (banner) {
         banner.classList.remove("game-banner-clicked");
         banner.firstElementChild.classList.remove("loading-div-anim");
-        
       });
-      bannerImgs.forEach(function(bannerImg){
-        bannerImg.classList.remove('small-game-img-selected');
-      })
+      bannerImgs.forEach(function (bannerImg) {
+        bannerImg.classList.remove("small-game-img-selected");
+      });
       banner.classList.add("game-banner-clicked");
       banner.firstElementChild.classList.add("loading-div-anim");
-      bannerImgs[index].classList.add('small-game-img-selected');
+      bannerImgs[index].classList.add("small-game-img-selected");
 
       currg.scrollTo({
         left: currg.clientWidth * index,
-        behavior: 'smooth'
-      })
+        behavior: "smooth",
+      });
     }
   });
 }
@@ -53,55 +48,149 @@ for (let i = 0; i < 4; i++) {
   banners[i].firstElementChild.addEventListener("animationend", () => {
     banners[i].classList.remove("game-banner-clicked");
     banners[i].firstElementChild.classList.remove("loading-div-anim");
-    bannerImgs[i].classList.remove('small-game-img-selected');
+    bannerImgs[i].classList.remove("small-game-img-selected");
     if (i + 1 < 4) {
-      banners[i+1].click()
+      banners[i + 1].click();
       banners[i + 1].classList.add("game-banner-clicked");
-      banners[i + 1].firstElementChild.classList.add("loading-div-anim"); 
-      bannerImgs[i+1].classList.add('small-game-img-selected');
+      banners[i + 1].firstElementChild.classList.add("loading-div-anim");
+      bannerImgs[i + 1].classList.add("small-game-img-selected");
     }
   });
 }
 
 banners[0].click();
 
+// footer backToTop logic
+let backTopDiv = document.querySelector(".back-to-top-bttn");
+backTopDiv.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+// footer list logic
+let comDiv = document.querySelector(".com-div");
+let comUl = document.querySelector(".com-ul");
+
+
+comDiv.firstElementChild.addEventListener("click", function () {
+  comUl.classList.toggle('show')
+});
+
+let osDiv = document.querySelector(".os-div");
+let osUl = document.querySelector(".os-ul");
+
+
+osDiv.firstElementChild.addEventListener("click", function () {
+  osUl.classList.toggle('show')
+});
+
+let mDiv = document.querySelector(".market-div");
+let mUl = document.querySelector(".Marketplace-ul");
+
+
+mDiv.firstElementChild.addEventListener("click", function () {
+  mUl.classList.toggle('show')
+});
 
 // swiper section
 
-let mainBannerWrapper = document.querySelector('.main-banner-wrapper');
-let mainSwiper = document.querySelector('.game-offers-section');
-let searchBarCont = document.querySelector('.searchbar-container')
-mainSwiper.style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`
-searchBarCont.style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`
-const swiper = new Swiper('.swiper', {
-  speed: 1200,
+let mainBannerWrapper = document.querySelector(".main-banner-wrapper");
+let mainSwiper = document.querySelectorAll(".game-offers-section");
+let searchBarCont = document.querySelector(".searchbar-container");
+let gameColumns = document.querySelector(".game-columns-section");
+mainSwiper[0].style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`;
+mainSwiper[1].style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`;
+searchBarCont.style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`;
+gameColumns.style.width = `min(${mainBannerWrapper.offsetWidth}px,100%)`;
+
+const swiper1 = new Swiper(".swiper1", {
+  speed: 900,
   slidesPerView: 4,
   slidesPerGroup: 4,
   spaceBetween: 1,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
   },
-    breakpoints: {
-    320: {
+  pagination: {
+    el: ".swiper-pagination1",
+  },
+  breakpoints: {
+    319: {
       slidesPerView: 1,
       slidesPerGroup: 1,
-      spaceBetween: 10
+      spaceBetween: 10,
     },
-    640: {
+    730: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 15
+      spaceBetween: 15,
     },
-    1024: {
+    1070: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
-    1400: {
+    1426: {
       slidesPerView: 4,
       slidesPerGroup: 4,
-      spaceBetween: 20
-    }
-  }
+      spaceBetween: 20,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".swiper2", {
+  speed: 900,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  pagination: {
+    el: ".swiper-pagination2",
+  },
+  breakpoints: {
+    319: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+    730: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 15,
+    },
+    1070: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+    },
+    1426: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const swiper3 = new Swiper(".swiper3", {
+  speed: 300,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  spaceBetween: 1,
+  pagination: {
+    el: ".swiper-pagination3",
+  },
+  breakpoints: {
+    319: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+    730: {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      spaceBetween: 15,
+    },
+  },
 });
